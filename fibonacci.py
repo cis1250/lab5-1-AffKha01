1d38a791ded10aa3c40f69f64e06a270ade6348e
AffKha01 <affan01@uoguelph.ca>
2025-10-20 21:39:00 -0400

    Implement user input and Fibonacci sequence functions
    Added user input function and modified Fibonacci logic.

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,34 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+def user_input():
+  while (True):
+    print("Please enter the number of terms")
+    terms = input()
+    if (terms.isdigit()):
+        terms = int(terms)
+        if (terms > 0):
+          break;
+        else:
+          print("Incorrect input, please input a integer greater than 0")
+    else:
+      print("Incorrect input, please input an integer greater than 0")
+  return terms
+
+def fibonacci (terms):
+  if (terms == 0):
+    return 0
+  if (terms == 1):
+    return 0
+  elif (terms == 2):
+    return 1
+  else:
+    return fibonacci(terms - 1) + fibonacci (terms - 2)
+
+def print_fib (terms):
+  for i in range (1, terms + 1):
+    print(fibonacci(i), end= " ")
+  
+terms = user_input()
+fibonacci(terms)
+print_fib(terms)
